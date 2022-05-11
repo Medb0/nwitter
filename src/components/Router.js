93d0f2e71682678184b9fa76e0f22fcdb80ef59a
Medb0 <bxcv1230@gmail.com>
2022-05-11 13:57:06 +0900

    Edit : Router.js

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
 import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
